90a5bf55d35f9cadceea24a899328429f39c7470
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-08-23 21:15:05 +0300

    Move to file

---
+++ items.js
@@ -0,0 +1,19 @@
+/*
+ * @prettier
+ */
+
+class Thing {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
+}
+
+class Feature extends Thing {
+  constructor(name, description, backgroundColor) {
+    super(name, description);
+    this.backgroundColor = backgroundColor;
+  }
+}
+
+export { Feature };
