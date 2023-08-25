4a4031d9632654aba0b8e1e0f3001c663ab76964
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-08-25 18:51:45 +0300

    Order by distance

--- src/pathfinder.ts
+++ src/pathfinder.ts
@@ -1,5 +1,19 @@
 import { AStarFinder } from 'astar-typescript';
 
+function calculateDistance(point1: Coords, point2: Coords) {
+  const dx = point2.x - point1.x;
+  const dy = point2.y - point1.y;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+function orderWaypointsByDistance(waypoints: Array<Waypoint>, referencePoint: Coords) {
+  return waypoints.slice().sort((waypoint1, waypoint2) => {
+    const distance1 = calculateDistance(referencePoint, waypoint1.coords);
+    const distance2 = calculateDistance(referencePoint, waypoint2.coords);
+    return distance1 - distance2;
+  });
+}
+
 class Pathfinder {
   private _walkableMatrix: MapArray;
   private aStarInstance: AStarFinder;
@@ -18,6 +32,10 @@ class Pathfinder {
 
   get matrix() {
     return this._walkableMatrix;
+  }
+
+  orderWaypointsEuclid(waypoints: Array<Waypoint>, coords: Coords) {
+    return orderWaypointsByDistance(waypoints, coords);
   }
 
   findPathTo(startPos: Coords, endPos: Coords) {
