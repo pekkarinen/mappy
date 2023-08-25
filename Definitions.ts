2443d38e0482dcf9a518232ce13c175938837b5d
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-08-25 15:35:15 +0300

    Add type definitions

---
+++ Definitions.ts
@@ -0,0 +1,32 @@
+type MapConfig = {
+  height: number;
+  width: number;
+  tileSize: number;
+};
+
+type Coords = {
+  x: number;
+  y: number;
+};
+
+type Feature = {
+  name?: string;
+  description?: string;
+  backgroundColor?: string;
+  border?: string;
+  className?: string;
+  text?: string;
+  textOrder?: number;
+};
+
+type Waypoint = {
+  name: string;
+  coords: Coords;
+  element: HTMLElement;
+};
+
+type MapFeature = {
+  feature: Feature;
+  coords: Coords;
+  element: HTMLElement;
+};
