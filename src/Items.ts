6b6eab9d183143c3176eb5307fa0f5f3c07232fd
Ville Pekkarinen <ville.pekkarinen@gmail.com>
2023-08-27 16:24:38 +0300

    Update Items

--- src/Items.ts
+++ src/Items.ts
@@ -1,9 +1,10 @@
 import type * as CSS from 'csstype';
+import { Coords } from './lib/types';
 
 export class Thing {
   name: string;
-  description: string;
-  constructor(name: string, description: string) {
+  description: string | null;
+  constructor(name: string, description: string | null) {
     this.name = name;
     this.description = description;
   }
@@ -11,7 +12,7 @@ export class Thing {
 
 export class Feature extends Thing {
   _appearance: CSS.Properties;
-  constructor(name: string, description: string, appearance: CSS.Properties) {
+  constructor(name: string, description: string | null, appearance: CSS.Properties) {
     super(name, description);
     this._appearance = appearance;
   }
@@ -21,7 +22,17 @@ export class Feature extends Thing {
   }
 }
 
-export class Waypoint extends Feature {}
+export class Waypoint extends Feature {
+  _coords: Coords;
+  constructor(name: string, appearance: CSS.Properties, coords: Coords) {
+    super(name, null, appearance);
+    this._coords = coords;
+  }
+
+  get coords() {
+    return this._coords;
+  }
+}
 
 export class Inventory {
   inventory: Array<Thing>;
